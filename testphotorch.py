d897c7001ba4b6b5a8513c67df3b303ae899ab2d
TongLei <tlei@ucdavis.edu>
2025-02-13 17:41:30 -0800

    v 1.3.0 Simplified function names and file structure. Updated the loss function for the FvCB model to prevent fitting failures in the light response curve.

--- testphotorch.py
+++ testphotorch.py
@@ -3,7 +3,7 @@ import stomatal
 import selftest
 import pandas as pd
 import torch
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 # selftest.run() # Check if all models are working
 
@@ -15,32 +15,32 @@ fitresult = fvcb.fit(fvcbm, learn_rate= 0.06, device='cpu', maxiteration = 20000
 fvcbm = fitresult.model
 fvcbm.eval()
 A_fit, Ac_fit, Aj_fit, Ap_fit = fvcbm()
+
 #plot all the data based on the ID
-plt.figure()
-for id in lcd.IDs:
-    if id == 118:
-        continue
-    indices_id = lcd.getIndicesbyID(id)
-    A_id = A_fit[indices_id]
-    # Ac_id = Ac_fit[indices_id]
-    # Aj_id = Aj_fit[indices_id]
-    # Ap_id = Ap_fit[indices_id]
-    plt.plot(lcd.Ci[indices_id],A_id.detach().numpy())
-    plt.plot(lcd.Ci[indices_id],lcd.A[indices_id],'.')
-plt.title('Fitted A/Ci curves')
-plt.show()
-
-plt.figure()
-indices_id = lcd.getIndicesbyID(118)
-# A_id = A_fit[indices_id]
-Ac_id = Ac_fit[indices_id]
-Aj_id = Aj_fit[indices_id]
-plt.plot(lcd.Q[indices_id],Ac_id.detach().numpy())
-plt.plot(lcd.Q[indices_id],Aj_id.detach().numpy())
-plt.plot(lcd.Q[indices_id],lcd.A[indices_id],'.')
-plt.title('Fitted Light response A/Q curve for ID 118')
-plt.show()
-
+# plt.figure()
+# for id in lcd.IDs:
+#     if id == 118:
+#         continue
+#     indices_id = lcd.getIndicesbyID(id)
+#     A_id = A_fit[indices_id]
+#     # Ac_id = Ac_fit[indices_id]
+#     # Aj_id = Aj_fit[indices_id]
+#     # Ap_id = Ap_fit[indices_id]
+#     plt.plot(lcd.Ci[indices_id],A_id.detach().numpy())
+#     plt.plot(lcd.Ci[indices_id],lcd.A[indices_id],'.')
+# plt.title('Fitted A/Ci curves')
+# plt.show()
+#
+# plt.figure()
+# indices_id = lcd.getIndicesbyID(118)
+# # A_id = A_fit[indices_id]
+# Ac_id = Ac_fit[indices_id]
+# Aj_id = Aj_fit[indices_id]
+# plt.plot(lcd.Q[indices_id],Ac_id.detach().numpy())
+# plt.plot(lcd.Q[indices_id],Aj_id.detach().numpy())
+# plt.plot(lcd.Q[indices_id],lcd.A[indices_id],'.')
+# plt.title('Fitted Light response A/Q curve for ID 118')
+# plt.show()
 
 # Stomatal model fitting
 datasc = pd.read_csv('exampledata/steadystate_stomatalconductance.csv')
@@ -50,10 +50,9 @@ scm = stomatal.fit(scm, learnrate = 0.5, maxiteration =20000)
 gsw = scm()
 gsw_mea = scd.gsw
 
-plt.figure()
-plt.plot(gsw_mea)
-plt.plot(gsw.detach().numpy(), '.')
-
-plt.title('Fitted gsw')
-plt.legend(['Fitted gsw', 'Measured gsw'])
-plt.show()
+# plt.figure()
+# plt.plot(gsw_mea)
+# plt.plot(gsw.detach().numpy(), '.')
+# plt.title('Fitted gsw')
+# plt.legend(['Fitted gsw', 'Measured gsw'])
+# plt.show()
